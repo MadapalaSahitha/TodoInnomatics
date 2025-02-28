b8211e118cd0a1e294ebbedfe5f068a50ce98c53
sahitha <madapalasahitha@gmail.com>
2025-02-28 14:32:39 +0530

    initial message

---
+++ Todo-App-main/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = { id: Date.now(), text };
+    setTodos([...todos, newTodo]);
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="app-container">
+      <h1>To-Do List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} removeTodo={removeTodo} />
+    </div>
+  );
+}
+
+export default App;
